b948b2601ba75128aaf45b08d78fd7ae3ffdb8f3
Ceomohan <ceomohan2004@gmail.com>
2024-12-28 14:01:42 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import path from 'path'
 
 
 import authRoutes from "./routes/authRoutes.js"
@@ -13,6 +14,9 @@ dotenv.config()
 
 const PORT = process.env.PORT || 5000 
 
+const __dirname = path.resolve()
+
+
 
 app.use(express.json())
 app.use(cookieParser())
@@ -20,6 +24,13 @@ app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
+
+app.use(express.static(path.join(__dirname,"/frontend/chat-app-yt/dist")))
+
+app.use("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","chat-app-yt","dist","index.html"))
+})
+
 
 server.listen(PORT ,()=>{
     connectMongoDb()
